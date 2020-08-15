0805b226c8e2c6c2965dfbe5f3e32bb8a2053982
valery-po <54510559+valery-po@users.noreply.github.com>
2020-08-15 21:01:25 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+$(function(){
+$(".photo__slider").slick({
+
+	prevArrow: '<button type="button" class="slick-prev"><img src="images/arrow-right.svg" alt=""></button>',
+	nextArrow: '<button type="button" class="slick-next"><img src="images/arrow-left.svg" alt=""></button>'
+});
+	
+	$(".menu__btn").on("click", function(){
+																				
+					$(".menu__list").slideToggle();												
+	})
+	
+});
